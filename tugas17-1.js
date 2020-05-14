38f19946f03779afd7dc746df716d4bf69d53062
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-05-14 20:49:46 +0700

    Tugas 17 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas17-1.js
@@ -0,0 +1,7 @@
+var a = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+var b = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
